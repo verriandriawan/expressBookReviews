2e39da8b8115cb6cef6dbbb5dcade7220ff2e086
Verri A <verri@tiduronline.com>
2024-08-10 22:43:35 +0700

    Task 5: Get review by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,8 +67,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let keys = Object.keys(books)
+  let items = keys.map(key => {
+    let item = books[key]
+    item.isbn = key
+    return item
+  })
+
+  const isbn = req.params.isbn
+  const book = items.find(item => item.isbn === isbn)
+  if (!book) {
+    return res.status(404).json({message: 'Book is not found'})
+  }
+
+  return res.status(200).json({isbn: book.isbn, title: book.title, reviews: book.reviews || []});
 });
 
 module.exports.general = public_users;
